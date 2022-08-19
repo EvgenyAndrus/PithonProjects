4f3e350067fadff9437e8dc4101a50094eec7c79
EvgenyAndrus <evg_an@mail.ru>
2022-08-19 11:48:57 +0300

    lesson4_MyRange with iterator

--- PythonEssential/lesson4/lesson4_MyRange.py
+++ PythonEssential/lesson4/lesson4_MyRange.py
@@ -20,13 +20,33 @@ class MyRange:
     def __len__(self):
         return self.length
 
-    def __getitem__(self, index):
-        if 0 <= index < len(self):
-            return self.start + index * self.step
-        else:
-            raise IndexError('MyRange index out of range')
+#    def __getitem__(self, index):
+#        if 0 <= index < len(self):
+#            return self.start + index * self.step
+#        else:
+#            raise IndexError('MyRange index out of range')
+
+    def __iter__(self):
+        return RangeIterator(self)
 
     def __repr__(self):
         return 'MyRange({}, {}, {})'.format(self.start, self.end, self.step)
 
-    
+
+class RangeIterator:
+    def __init__(self, range_instance):
+        self.range = range_instance
+        self.next_value = range_instance.start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.next_value >= self.range.end and self.range.step > 0 or \
+                self.next_value <= self.range.end and self.range.step < 0:
+            raise StopIteration
+
+        result = self.next_value
+        self.next_value += self.range.step
+
+        return result
